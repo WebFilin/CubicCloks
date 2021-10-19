d16cbbd48aeab2d7d83dfc9d14c2364dcc9e75a7
WebFilin <filimigr@gmail.com>
2021-10-19 19:45:44 +0500

    Работаем над логикой отрисовки

--- js/App.js
+++ js/App.js
@@ -90,6 +90,7 @@ let numbers = [
    ],
 ];
 
+// Поля для вывода значения времени
 let displHours1 = document.getElementById("displHours1");
 let displHours2 = document.getElementById("displHours2");
 let displMinuts = document.getElementById("displMinuts");
@@ -97,39 +98,59 @@ let displSeconds = document.getElementById("displSeconds");
 
 let cubes = document.getElementById('wrapCube');
 
-
-
 //Рисуем поле элементов по массиву ArrAll
 function createDisplClocs(createTable) {
-   let cells;
+
+   // Перебираем массив arrDispl
    for (i = 0; i < arrDispl.length; i++) {
+
+      // создаем строки таблицы для вывода кубиков
       let tr = document.createElement("tr");
+
+      // Перебираем массив arrDispl - получаем все значения
       for (j = 0; j < arrDispl[i].length; j++) {
-         cells = document.createElement("div");
+
+         // Создаем отдельные элементы - кубики цифр
+         let cells = document.createElement("div");
          cells.className = 'cells';
-         cells.innerHTML = arrDispl[i][j];
+         // Добавляем кубики в строки
          tr.appendChild(cells);
-         createTable.appendChild(tr);
+
+
+         createTable.append(tr);
          if (arrDispl[i][j] == 1) {
             let newCubes = cubes.cloneNode(true);
             newCubes.style.display = "block";
             cells.append(newCubes);
+            // console.log(newCubes)
          }
       }
+      // console.log(tr)
    }
-};
+
+
+}
+
+
 
 //Меняем элементы перебором массива numbers и заменой массива ArrAll  
-function changeSec(time) {
+function changeNumbers(time) {
    arrDispl.splice(0);
    Array.prototype.push.apply(arrDispl, numbers[time]);
+
 };
 
 
+let hour = 3;
+let hour2 = 2;
+let minuts = 3;
+let seconds = 4;
 
-createDisplClocs(displHours1, changeSec(1));
-createDisplClocs(displHours2, changeSec(2));
-createDisplClocs(displMinuts, changeSec(3));
-createDisplClocs(displSeconds, changeSec(4));
+
+createDisplClocs(displMinuts, changeNumbers(4));
+
+// createDisplClocs(displMinuts, changeNumbers(4));
+
+
 
 
