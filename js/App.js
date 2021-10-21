5856ea48594d0ee73e05a0361a4430b30a9d4094
WebFilin <filimigr@gmail.com>
2021-10-21 14:06:18 +0500

    Разобраться с динамическим обновлением цифры в блоке экрана, пока что программа добавляет просто еще одну рядом

--- js/App.js
+++ js/App.js
@@ -1,6 +1,4 @@
 
-
-
 let allDispls = {
    // Дисплеи для вывода значения времени
    displHours1: document.getElementById("displHours1"),
@@ -9,11 +7,13 @@ let allDispls = {
    displSeconds: document.getElementById("displSeconds"),
 }
 
+
+
 // Контейнер для одного элемента кубика
 let cubes = document.getElementById('wrapCube');
 
 //Рисуем поле элементов по массиву ArrAll и собираем цифру указанную в цункции changeNumbers
-function createDisplClocks(displ, time) {
+let createDisplClocks = (displ, time) => {
 
    // Перебираем массив arrDispl
    for (i = 0; i < arrDispl.length; i++) {
@@ -29,7 +29,6 @@ function createDisplClocks(displ, time) {
          // Добавляем кубики в строки
          tr.append(cells);
 
-         // Добавляем результат выполнения функции в аргумент функции  
          displ.append(tr);
 
          // Получаем конкретную цифру для отрисовки - перерисовываем массив arrDispl по массиву numbers
@@ -46,68 +45,35 @@ function createDisplClocks(displ, time) {
    }
 }
 
-let hours = 0;
-let hours2 = 0;
-let minuts = 0;
-let seconds = 0;
+let hours = 1;
+let hours2 = 2;
+let minuts = 3;
+let seconds = 4;
 
-function aaa() {
-   hours = ++hours;
-   minuts = ++minuts;
+function clock() {
+   now = new Date();
+   h = now.getHours();
+   m = now.getMinutes() / 10;
+   s = now.getSeconds() / 10;
 }
 
-console.log("переменная " + hours)
+function showTime() {
 
-createDisplClocks(allDispls.displHours1, hours);
-createDisplClocks(allDispls.displHours2, hours2);
-createDisplClocks(allDispls.displMinuts, minuts);
-createDisplClocks(allDispls.displSeconds, seconds)
+   createDisplClocks(allDispls.displHours1, hours);
+   createDisplClocks(allDispls.displHours2, hours2);
+   createDisplClocks(allDispls.displMinuts, minuts);
+   createDisplClocks(allDispls.displSeconds, seconds);
 
+}
 
-
-
-
-
+showTime()
 
 
 
 
-// var img = new Array(
-//    "image/time/img0.gif",
-//    "image/time/img1.gif",
-//    "image/time/img2.gif",
-//    "image/time/img3.gif",
-//    "image/time/img4.gif",
-//    "image/time/img5.gif",
-//    "image/time/img6.gif",
-//    "image/time/img7.gif",
-//    "image/time/img8.gif",
-//    "image/time/img9.gif");
 
-// function extract(type, hr, min, sec) {
-//    if (hr <= 9) {
-//       document.getElementById("h10").src = img[0];
-//       document.getElementById("h1").src = img[hr];
-//    }
-//    else {
-//       document.getElementById("h10").src = img[Math.floor(hr / 10)];
-//       document.getElementById("h1").src = img[hr % 10];
-//    }
-//    if (min <= 9) {
-//       document.getElementById("m10").src = img[0];
-//       document.getElementById("m1").src = img[min];
-//    }
-//    else {
-//       document.getElementById("m10").src = img[Math.floor(min / 10)];
-//       document.getElementById("m1").src = img[min % 10];
-//    }
-//    if (sec <= 9) {
-//       document.getElementById("s10").src = img[0];
-//       document.getElementById("s1").src = img[sec];
-//    }
-//    else {
-//       document.getElementById("s10").src = img[Math.floor(sec / 10)];
-//       document.getElementById("s1").src = img[sec % 10];
-//    }
 
-// }
+
+
+
+
