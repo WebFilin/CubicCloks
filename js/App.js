b278aef6c95f12ad3ce50f57dacf1a41c529fbb2
WebFilin <filimigr@gmail.com>
2021-10-23 15:22:30 +0500

    Запустили отсчет времени и предварительную очистку цифры на табло перед перерисовкой

--- js/App.js
+++ js/App.js
@@ -1,25 +1,35 @@
 
-let allDispls = {
-   // Дисплеи для вывода значения времени
-   displHours: document.querySelector(".base-displ__hours"),
-   displHours2: document.querySelector(".base-displ__hours2 "),
-   displMinuts: document.querySelector(" .base-displ__minuts "),
-   displMinuts2: document.querySelector(" .base-displ__minuts2 "),
-   displSeconds: document.querySelector(" .base-displ__seconds "),
-   displSeconds2: document.querySelector(" .base-displ__seconds2 ")
-}
+
+// Дисплеи для вывода значения времени
+displHours = document.querySelector(".base-displ__hours");
+displHours2 = document.querySelector(".base-displ__hours2 ");
+displMinuts = document.querySelector(" .base-displ__minuts ");
+displMinuts2 = document.querySelector(" .base-displ__minuts2 ");
+displSeconds = document.querySelector(" .base-displ__seconds ");
+displSeconds2 = document.querySelector(" .base-displ__seconds2 ");
+
 
 // Контейнер для одного собраного элемента кубика
 let cubes = document.querySelector('.cubes-wrap');
 
 //Рисуем поле элементов по массиву ArrAll и собираем цифру указанную в цункции changeNumbers
-let createDisplClocks = (displ) => {
+function createDisplClocks(displ, time) {
+
+   // Очищаем таблицу экрана displ перед заполнением значениями
+   while (displ.rows.length) {
+      displ.deleteRow(0);
+   }
+
+   // Получаем конкретную цифру для отрисовки - перерисовываем массив arrDispl по массиву numbers
+   arrDispl.splice(0);
+   Array.prototype.push.apply(arrDispl, numbers[time]);
 
    // Добавляем класс для запуска анимации кубика
-   cubes.classList.add("cubes__animo-flip");
+   // cubes.classList.add("cubes__animo-flip");
 
    // Перебираем массив arrDispl
    for (i = 0; i < arrDispl.length; i++) {
+
       // создаем строки таблицы для вывода кубиков
       let tr = document.createElement("tr");
 
@@ -30,75 +40,46 @@ let createDisplClocks = (displ) => {
          let cells = document.createElement("div");
          cells.classList.add('cells');
 
+         // Отрисовываем нужный сегмент цифры по массиву numbers
+         if (arrDispl[i][j] == 1) {
+            let newCubes = cubes.cloneNode(true);
+            cells.append(newCubes);
+         }
+
          // Добавляем кубики в строки
          tr.append(cells);
 
          // Добаляем сформированную цифру на экран через аргумент displ
          displ.append(tr);
-
-         // Отрисовываем нужный сегмент цифры по массиву numbers
-         if (arrDispl[i][j] == 1) {
-            let newCubes = cubes.cloneNode(true);
-            newCubes.style.display = "block";
-            cells.append(newCubes);
-         }
       }
    }
 }
 
-// Получаем конкретную цифру для отрисовки - перерисовываем массив arrDispl по массиву numbers
-function createNumber(time) {
-   arrDispl.splice(0);
-   Array.prototype.push.apply(arrDispl, numbers[time]);
-}
-
-// Переменные для передачи значения времени в функцию createNumber
-// let hours = 0;
-// let hours2 = 0;
-// let minutes = 0;
-// let minutes2 = 0;
-let seconds = 0;
-// let seconds2 = 0;
 
 // Устанавливаем значение времени
 function currentTime() {
    let data = new Date();
-   let timeHours = data.getHours();
-   let timeMinuts = data.getMinutes();
-   let timeSeconds = data.getSeconds();
+   let hours = data.getHours();
+   let minuts = data.getMinutes();
+   let seconds = data.getSeconds();
 
-   let time = {
-      hours: timeHours,
-      hours2: timeHours,
-      minutes: timeMinuts,
-      minutes2: timeMinuts,
-      seconds: timeSeconds,
-      seconds2: timeSeconds,
-   };
-
-   console.log("Секунды " + time.seconds);
+   console.log("Секунды " + seconds);
 
    setTimeout(() => {
       currentTime()
    }, 1000);
 
+   createDisplClocks(displHours, seconds);
+   createDisplClocks(displHours2, seconds);
+
+   createDisplClocks(displMinuts, seconds);
+   createDisplClocks(displMinuts2, seconds);
+
+   createDisplClocks(displSeconds, seconds);
+   createDisplClocks(displSeconds2, seconds);
 
 }
-
-
-// createNumber(hours);
-// createDisplClocks(allDispls.displHours);
-// createNumber(hours2);
-// createDisplClocks(allDispls.displHours2);
-// createNumber(minutes);
-// createDisplClocks(allDispls.displMinuts);
-// createNumber(minutes2);
-// createDisplClocks(allDispls.displMinuts2);
-// createNumber(seconds);
-
-createDisplClocks(allDispls.displSeconds);
-// createNumber(seconds2);
-// createDisplClocks(allDispls.displSeconds2);
+// currentTime()
 
 
 
