1466c8f1c784670d1b4f744765a60a2034278c89
WebFilin <filimigr@gmail.com>
2021-10-26 16:25:14 +0500

    Настраиваем логику отображения анимации - 1

--- js/App.js
+++ js/App.js
@@ -6,6 +6,10 @@ const displMinuts2 = document.querySelector(" .base-displ__minuts2 ");
 const displSeconds = document.querySelector(" .base-displ__seconds ");
 const displSeconds2 = document.querySelector(" .base-displ__seconds2 ");
 
+const btnStart = document.querySelector(".button-control__start ")
+const btnStop = document.querySelector(".button-control__stop ")
+
+
 //Создаем контейнер для одного собраного элемента кубика
 const cubes = document.createElement("div");
 cubes.classList.add("cubes", "cubes-wrap");
@@ -17,7 +21,81 @@ const cubesSides = `
 		<div class="front side"></div>
 `;
 
+// Создаем один полный кубик
 cubes.insertAdjacentHTML("afterbegin", cubesSides);
+
+// Устанавливаем значение времени
+function currentTime() {
+
+   // Получаем дату
+   let data = new Date();
+   let hours = data.getHours();
+   let minuts = data.getMinutes();
+   let seconds = data.getSeconds();
+
+   displayControl(hours, minuts, seconds);
+
+}
+
+currentTime()
+
+// Управляем выводом времени на дисплеи - на каждый дисплей (их два на каждую цифру) пердаем свою цифру индивидуально
+function displayControl(hours, minuts, seconds) {
+
+   // Переформатруем время и разделяем его на два значения для отображения на каждом из 6 табло
+   const displsTime = {
+      displH1: Number(hours.toString().slice(0, 1)),
+      displH2: Number(hours.toString().slice(-1,)),
+      displMin1: Number(minuts.toString().slice(0, 1)),
+      displMin2: Number(minuts.toString().slice(-1,)),
+      displSec1: Number(seconds.toString().slice(0, 1)),
+      // displSec2: Number(seconds.toString().slice(-1,)),
+
+      get displSec2() {
+         console.log("get!")
+         animoGet(displSeconds2)
+         return Number(seconds.toString().slice(-1,))
+      }
+   }
+
+   // Логика отображения времени - устанавливаем 0 на первом циферблате при полученном одинарном числе
+   if (hours > 9) {
+      createNumber(displHours, displsTime.displH1);
+      createNumber(displHours2, displsTime.displH2);
+   }
+   else {
+      createNumber(displHours, displsTime.displH1 = 0);
+      createNumber(displHours2, displsTime.displH2);
+   }
+
+   if (minuts > 9) {
+      createNumber(displMinuts, displsTime.displMin1);
+      createNumber(displMinuts2, displsTime.displMin2);
+   }
+   else {
+      createNumber(displMinuts, displsTime.displMin1 = 0);
+      createNumber(displMinuts2, displsTime.displMin2);
+   }
+
+   if (seconds > 9) {
+      createNumber(displSeconds, displsTime.displSec1);
+      createNumber(displSeconds2, displsTime.displSec2);
+   }
+
+   else {
+      createNumber(displSeconds, displsTime.displSec1 = 0);
+      createNumber(displSeconds2, displsTime.displSec2);
+   }
+
+
+
+   function animoGet(displ) {
+      console.log("animoGet сработала")
+      addAnimoRotation(displ)
+   }
+
+}
+
 
 //Рисуем поле элементов по массиву ArrAll и собираем цифру указанную в цункции changeNumbers
 function createNumber(displ, time) {
@@ -56,86 +134,20 @@ function createNumber(displ, time) {
          displ.append(tr);
       }
    }
-}
-
-
-// Управляем выводом времени на дисплеи - на каждый дисплей (их два на каждую цифру) пердаем свою цифру индивидуально
-function displayControl(hours, minuts, seconds) {
-
-   // Переформатруем время и разделяем его на два значения для отображения на каждом из 6 табло
-   const displsTime = {
-      displH1: Number(hours.toString().slice(0, 1)),
-      displH2: Number(hours.toString().slice(-1,)),
-      displMin1: Number(minuts.toString().slice(0, 1)),
-      displMin2: Number(minuts.toString().slice(-1,)),
-      displSec1: Number(seconds.toString().slice(0, 1)),
-      // displSec2: Number(seconds.toString().slice(-1,)),
-
-      get displSec2() {
-         console.log('get!');
-         return Number(seconds.toString().slice(-1,));
-      },
-   }
-
-   // Логика отображения времени - устанавливаем 0 на первом циферблате при полученном одинарном числе
-   if (hours > 9) {
-      createNumber(displHours, displsTime.displH1);
-      createNumber(displHours2, displsTime.displH2);
-   }
-   else {
-      createNumber(displHours, displsTime.displH1 = 0);
-      createNumber(displHours2, displsTime.displH2);
-   }
-
-   if (minuts > 9) {
-      createNumber(displMinuts, displsTime.displMin1);
-      createNumber(displMinuts2, displsTime.displMin2);
-   }
-   else {
-      createNumber(displMinuts, displsTime.displMin1 = 0);
-      createNumber(displMinuts2, displsTime.displMin2);
-   }
-
-   if (seconds > 9) {
-      createNumber(displSeconds, displsTime.displSec1);
-      createNumber(displSeconds2, displsTime.displSec2);
-
-   }
-   else {
-      createNumber(displSeconds, displsTime.displSec1 = 0);
-      createNumber(displSeconds2, displsTime.displSec2);
-   }
 
 }
 
-// Устанавливаем значение времени
-function currentTime() {
-
-   // Получаем дату
-   let data = new Date();
-   let hours = data.getHours();
-   let minuts = data.getMinutes();
-   let seconds = data.getSeconds();
-
-   displayControl(hours, minuts, seconds);
-
-   // setTimeout(() => {
-   //    currentTime()
-   // }, 1000);
-
-}
-
-currentTime();
-
-addAnimoRotation(displSeconds2);
 
 // Управление анимацией - запускаем только при изменении цифры на табло
 function addAnimoRotation(cubesOnDispl) {
 
-   console.log("addAnimoRotation woorck");
+   console.log("Анимация сработала");
 
    // Получаем все кубики - на данном дисплее
    let cubesLists = cubesOnDispl.querySelectorAll(".cubes");
+
+   // console.log(cubesOnDispl);
+   console.log(cubesLists);
 
    // Добавляем класс анимации
    cubesLists.forEach((item) => {
@@ -143,17 +155,25 @@ function addAnimoRotation(cubesOnDispl) {
    })
 
    // Удаляем класс анимации
-   cubesLists.forEach((item) => {
-      item.addEventListener('animationend', function () {
-         item.classList.remove('cubes__animo-flip');
-      })
-   })
+   // cubesLists.forEach((item) => {
+   //    item.addEventListener('animationend', function () {
+   //       item.classList.remove('cubes__animo-flip');
+   //    })
+   // })
 }
 
+// Кнопки управлени часами
+let intervalTime;
 
+btnStart.addEventListener("click", () => {
+   intervalTime = setInterval(() => {
+      currentTime();
+   }, 1000);
+})
 
-
-
+btnStop.addEventListener("click", () => {
+   clearInterval(intervalTime);
+})
 
 
 
